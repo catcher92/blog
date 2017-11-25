3927f11563ef913f57c328257f6e3d289cbc0f50
catcher92 <catcher92@163.com>
2017-11-25 21:10:24 +0800

    修改包名

--- src/main/java/com/catcher92/blog/config/WebSecurityConfig.java
+++ src/main/java/com/catcher92/blog/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.catcher92.bolg.config;
+package com.catcher92.blog.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
--- src/test/java/com/catcher92/blog/BlogApplicationTests.java
+++ src/test/java/com/catcher92/blog/BlogApplicationTests.java
@@ -1,4 +1,4 @@
-package com.catcher92.bolg;
+package com.catcher92.blog;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BolgApplicationTests {
+public class BlogApplicationTests {
 
 	@Test
 	public void contextLoads() {
